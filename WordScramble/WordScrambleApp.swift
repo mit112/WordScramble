99e52a7b8d2460287d0b34e4ebdbd8e16848c407
Mit Kamal Sheth <sheth.mit@northeastern.edu>
2024-01-19 15:14:13 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Mit Sheth on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
